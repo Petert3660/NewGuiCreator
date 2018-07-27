d50da1afcbd0a1486b50b133d8eb7e715de29e2e
Petert3660 <39087631+Petert3660@users.noreply.github.com>
2018-07-27 08:49:03 +0100

    master
    Tests added to cover FreeLabelComboPair

--- src/test/java/com/thehutgroup/guicomponents/TestComponents.java
+++ src/test/java/com/thehutgroup/guicomponents/TestComponents.java
@@ -3,6 +3,7 @@ package com.thehutgroup.guicomponents;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import org.junit.Before;
 import org.junit.Test;
@@ -29,7 +30,7 @@ public class TestComponents {
     @Test
     public void testComponenents_comboBox() {
 
-        String[] NEW_ITEMS = {"--Select", "New_Item_1", "New_Item_2"};
+        final String[] NEW_ITEMS = {"--Select", "New_Item_1", "New_Item_2"};
 
         FreeComboBox fcb = new FreeComboBox(0, 0, 0, 0, items);
 
@@ -52,5 +53,40 @@ public class TestComponents {
         assertThat(fcb.getItem(0), is(NEW_ITEMS[0]));
         assertThat(fcb.getItem(1), is(NEW_ITEMS[1]));
         assertThat(fcb.getItem(2), is(NEW_ITEMS[2]));
+    }
+
+    @Test
+    public void testComponenents_label_comboBox_pair() {
+
+        final String LABEL_TEXT = "Blah blah";
+        final String NEW_LABEL_TEXT = "New label";
+        final String[] NEW_ITEMS = {"--Select", "New_Item_1", "New_Item_2"};
+
+        FreeLabelComboBoxPair fcbp = new FreeLabelComboBoxPair(new Color(0, 0, 0), LABEL_TEXT, 0, 0, 0, 0, items);
+
+        assertThat(fcbp.getComboBox().getItemCount(), is(4));
+        assertThat(fcbp.getComboBox().getItem(0), is(ITEM_0));
+        assertThat(fcbp.getComboBox().getItem(1), is(ITEM_1));
+        assertThat(fcbp.getComboBox().getItem(2), is(ITEM_2));
+        assertThat(fcbp.getComboBox().getItem(3), is(ITEM_3));
+        fcbp.clearComboBox();
+        assertThat(fcbp.getComboBox().getItemCount(), is(4));
+        assertThat(fcbp.getComboBox().getSelectedItem(), is(ITEM_0));
+        assertThat(fcbp.isFirstItemSelected(), is(true));
+        assertThat(fcbp.getSelectedItem(), is(ITEM_0));
+        assertThat(fcbp.getLabelText(), is(LABEL_TEXT));
+        fcbp.setLabelText(NEW_LABEL_TEXT);
+        assertThat(fcbp.getLabelText(), is(NEW_LABEL_TEXT));
+
+        items = new ArrayList<>();
+        for (int i = 0; i < NEW_ITEMS.length; i++) {
+            items.add(NEW_ITEMS[i]);
+        }
+        fcbp.repopulateComboBox(items);
+
+        assertThat(fcbp.getComboBox().getItemCount(), is(3));
+        assertThat(fcbp.getComboBox().getItem(0), is(NEW_ITEMS[0]));
+        assertThat(fcbp.getComboBox().getItem(1), is(NEW_ITEMS[1]));
+        assertThat(fcbp.getComboBox().getItem(2), is(NEW_ITEMS[2]));
     }
 }
